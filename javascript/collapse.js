84de20c8a50802e1962e25d155be16c7f18ec018
thangplc <thangplc1412@gmail.com>
2021-05-31 23:55:21 +0700

    completed responsive

--- javascript/collapse.js
+++ javascript/collapse.js
@@ -1,13 +1,8 @@
-$(document).ready(function() {
-    // Add minus icon for collapse element which is open by default
-    $(".collapse.show").each(function() {
-        $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
-    });
-
-    // Toggle plus minus icon on show hide of collapse element
-    $(".collapse").on('show.bs.collapse', function() {
-        $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
-    }).on('hide.bs.collapse', function() {
-        $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
-    });
-}); <
+$('.panel-collapse').on('show.bs.collapse', function() {
+    $(this).parent('.panel').find('.fa-minus').show();
+    $(this).parent('.panel').find('.fa-plus').hide();
+})
+$('.panel-collapse').on('hide.bs.collapse', function() {
+    $(this).parent('.panel').find('.fa-minus').hide();
+    $(this).parent('.panel').find('.fa-plus').show();
+})
